4fe41fc26e580d5f6ad220b4a7c19cfc2c65ae81
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-02-02 20:16:33 -0800

    Update api.js
    This is for the JS folk to see

--- api.js
+++ api.js
@@ -1,1 +1,19 @@
-
+//use this "http://api.dataatwork.org/v1/jobs/autocomplete?contains=" + search-entry; 
+fetch("http://api.dataatwork.org/v1/jobs/autocomplete?contains=teacher").then(function(response){
+    if(response.ok){
+        response.json().then(function(apidata){
+            console.log("Jobs:");
+            console.log(apidata);
+        })
+    }
+})
+// use this "http://api.dataatwork.org/v1/skills/autocomplete?contains=" + skills-search-entry;
+//this appears to list skill only with no reference to the job?
+fetch("http://api.dataatwork.org/v1/skills/autocomplete?contains=javascript").then(function(response){
+    if(response.ok){
+        response.json().then(function(apidata){
+            console.log("skills:")
+            console.log(apidata);
+        })
+    }
+})

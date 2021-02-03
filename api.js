f9cf19c5d7c797350735d3169add4ce0e85bc512
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-02-02 21:26:06 -0800

    Update api.js

--- api.js
+++ api.js
@@ -19,8 +19,21 @@ fetch("http://api.dataatwork.org/v1/jobs/autocomplete?contains=teacher").then(fu
         })
     }
 })
+
+// uuid is the attribute we want from the jobs ^^^ to plug into this one for related skills
+//From the array we get we go skills -> which gives objects of skills (we want) skill_name & description 
+//for each skill object.
+fetch("http://api.dataatwork.org/v1/jobs/2c92effbbca763fc5b05c0afaee2d3ea/related_skills").then(function(response){
+    if(response.ok){
+        response.json().then(function(apidata){
+            console.log("Related skills");
+            console.log(apidata);
+        })
+    }
+})
+
 // use this "http://api.dataatwork.org/v1/skills/autocomplete?contains=" + skills-search-entry;
-//this appears to list skill only with no reference to the job?
+//this appears to list skills only with no reference to the job?
 fetch("http://api.dataatwork.org/v1/skills/autocomplete?contains=javascript").then(function(response){
     if(response.ok){
         response.json().then(function(apidata){

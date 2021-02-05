692c0f90fae20259c57ec69c8967bf83079a3eb0
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-02-04 19:53:42 -0800

    Update api.js
    Have working console log of API call data navigation with temporary variable names

--- api.js
+++ api.js
@@ -1,4 +1,46 @@
+//global var because I can't figure out how to use .then return values
+var uuids = [];
+//use this "http://api.dataatwork.org/v1/jobs/autocomplete?contains=" + search-entry; 
+fetch("http://api.dataatwork.org/v1/jobs/autocomplete?contains=teacher").then(function(response){
+    if(response.ok){
+        response.json().then(function(apidata){
+            console.log("Jobs:");
+            console.log(apidata);
+            for (var i = 0; i<10; i++){
+                uuids.push(apidata[i].uuid);
+            }
+         //This .then was to make sure the .push above was functioning properly 
+         //Remove for actual implementation
+        }).then(function(){
+            console.log("uuids x10");
+            for(var i = 0; i<uuids.length;i++){
+                console.log(uuids[i]);
+            }
+        })
+        //end remove
+    }
+    
+})
 
+for(var i =0; i<10; i++){
+    fetch("http://api.dataatwork.org/v1/jobs/" + uuids[i] + "/related_skills").then(function(response){
+        if(response.ok){
+            response.json().then(function(apidata){
+                console.log("Related skills");
+                for(var i = 0; i<10){
+                     var randomI= Math.floor(Math.random()*apidata.skills.length)
+                console.log(apidata.skills[randomI].skill_name);
+                console.log(apidata.skills[randomI].description);
+            }
+        })
+        }
+    })
+}
+
+
+
+
+//Everything after this point is reference
 //Can default a blank search to pull up random jobs. limit is 500.
 fetch("http://api.dataatwork.org/v1/jobs?limit=100").then(function(response){
     if(response.ok){
